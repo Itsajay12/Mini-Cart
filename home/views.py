be597972ecb5b45eacd3afabc5c199d96dea0f26
itsajay12 <ajayv570040@gmail.com>
2023-11-23 16:05:54 +0530

    icons added

--- home/views.py
+++ home/views.py
@@ -5,3 +5,5 @@ def index(request):
     return render(request,'index.html')
 def register(request):
     return render(request,'register.html')
+def login(request):
+    return render(request,'login.html')
