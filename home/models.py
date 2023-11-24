c1408834b0bf89d91e13323c84b08be50571a315
itsajay12 <ajayv570040@gmail.com>
2023-11-24 18:12:18 +0530

    new model created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class DeliveryAddress(models.Model):
+    username=models.ForeignKey(User,on_delete=models.CASCADE)
+    mobile=models.IntegerField(unique=True)
+    altmob=models.IntegerField(unique=True)
+    pincode=models.IntegerField()
+    address=models.TextField()
+    state=models.CharField(max_length=30)
+    country=models.CharField(max_length=30)
